dedb549e5a49bf458929b4533a8025386263fd74
senoam <seno.adhimuhammad@gmail.com>
2021-11-22 18:53:07 -0800

    Add login page

---
+++ client/src/routes/login/login.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from 'react';
+import './login.css'
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() { 
+        return(
+            <Fragment>
+            <div className="login-wrapper">
+            <h2>Login</h2>
+            <form>
+                <label>
+                    <p>Username</p>
+                    <input type="text" />
+                </label>
+                <label>
+                    <p>Password</p>
+                    <input type="password" />
+                </label>
+                <br/>
+                <div>
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+            </div>
+            </Fragment>
+        );
+    }
+}
+
+export default Login;
