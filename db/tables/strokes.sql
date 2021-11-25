106ce1333beba21359d708d5807f02cbd84c21ef
Jennifer Dai <jydai@sfu.ca>
2021-11-24 19:35:38 -0800

    fix missing comma

--- db/tables/strokes.sql
+++ db/tables/strokes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS strokes (
     stroke_id serial,
     whiteboard_id int,
-    user_id int
+    user_id int,
     stroke_time timestamp(3),
     PRIMARY KEY (stroke_id),
     FOREIGN KEY (whiteboard_id) REFERENCES whiteboard(whiteboard_id),
