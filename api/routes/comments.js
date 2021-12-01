575251d0dbcece54dc48ba8cbab383e5b74e869c
Seno Adhi Muhammad <samuhamm@sfu.ca>
2021-11-30 17:38:46 -0800

    Implement insert comment to db API

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -10,4 +10,30 @@ router.get("/db", function (req, res) {
         .catch(() => res.send("Theres something wrong with user table."));
 });
 
+router.post("/db", function (req, res) {
+    var whiteboardID = req.body.whiteboardID;
+    var commentLocation = req.body.commentLocation;
+    var messageText = req.body.messageText;
+    var userID = req.body.userID;
+    var parentCommentID = req.body.parentCommentID;
+    var timestamp = req.body.timestamp;
+
+    req.db.query(
+        `INSERT INTO comments(whiteboard_id, comment_location, message_text, user_id, parent_comment_id, time_stamp) VALUES ($1, $2, $3, $4, $5, $6)`,
+        [
+            whiteboardID,
+            commentLocation,
+            messageText,
+            userID,
+            parentCommentID,
+            timestamp
+        ],
+        (err, result) => {
+            if (err) {
+                throw err;
+            }
+        }
+    );
+});
+
 module.exports = router;
