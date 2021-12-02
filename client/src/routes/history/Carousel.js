24474e5e84beddea2b7889bc1baac160dc44d430
tonyq <tony_quan@sfu.ca>
2021-12-01 23:07:49 -0800

    Debugging browserify, image saving

--- client/src/routes/history/Carousel.js
+++ client/src/routes/history/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import axios from "axios";
 
@@ -11,13 +11,22 @@ function Carousel(props) {
 						room_id: room
 					})
 					.then((response) => {
-						console.log(response.data);
+						console.log(response.data.data);
+                        
+                        for (var i = 0; i < response.data.data.length; i ++){
+                            var fs = require('browserify-fs');
+
+                            fs.readFile('/history/' + response.data.data[i].image_id +'.png', 'base64', (err, data) => {
+								if (err) throw err;
+								console.log("Image data: " + data);
+							  });
+                        }
                     });
     };
 
     useEffect(() => {
 		getHistory();
-    });
+    }, []);
 
     return (
         <h1>Snapshot History</h1>
