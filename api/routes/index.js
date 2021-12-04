04e17a7bd801840dbc4688ea064f646648e5b286
Seno Adhi Muhammad <samuhamm@sfu.ca>
2021-12-04 07:50:57 +0700

    Add auth token to the homepage

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
+var auth = require("../modules/auth");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.get("/", auth.verifyToken, function (req, res, next) {
     res.render("index", { title: "Express" });
 });
 
