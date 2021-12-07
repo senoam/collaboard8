5b7b758eb0a5b38037ae33beb8c180dc47c6dca7
jydai <jennifer_dai@sfu.ca>
2021-12-07 23:08:59 +0000

    Add strokes database basic functionality

--- client/src/routes/canvas/tools/strokeData.js
+++ client/src/routes/canvas/tools/strokeData.js
@@ -1,24 +1,32 @@
+import axios from "axios";
 import { ToolManager } from "./ToolManager";
 
 // deals with all stroke database and socket interactions
 
 export const sendStroke = (socketObj, shape, data_string) => {
-    var formData = new FormData();
-    formData.append("shape", shape);
-    formData.append("data", data_string);
-    // TODO(jen): send stroke to database
     socketObj.socket.emit("drawing", socketObj.room, {
-        shape: shape,
-        data: data_string
+        brush_shape: shape,
+        data_string: data_string
     });
-    console.log("sending stroke", formData.getAll("shape"), formData.getAll("data"));
+    console.log("sending stroke", shape, data_string);
+
+    axios.post("http://localhost:4200/strokes/save", {
+        whiteboard_id: socketObj.room,
+        data_string: data_string,
+        brush_shape: shape
+    });
 };
 
 export const retrieveStroke = (socketObj) => {
-    // TODO(jen): get strokes from database
     const toolManager = new ToolManager(socketObj);
     socketObj.socket.on("drawing", (stroke) => {
-        console.log(stroke.shape, stroke.data);
-        toolManager.load(stroke.shape, stroke.data);
+        console.log(stroke.brush_shape, stroke.data_string);
+        toolManager.load(stroke.brush_shape, stroke.data_string);
+    });
+
+    axios.get("http://localhost:4200/strokes/get/" + socketObj.room).then((response) => {
+        for (var stroke of response.data) {
+            toolManager.load(stroke.brush_shape, stroke.data_string);
+        }
     });
 };
