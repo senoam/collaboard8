9d2a085ce32714afc2826030a40bdfdab5ff0800
Seno Adhi Muhammad <samuhamm@sfu.ca>
2021-12-08 08:25:24 +0700

    Update signup button on login page

--- client/src/routes/login/login.js
+++ client/src/routes/login/login.js
@@ -3,16 +3,6 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 import "./login.css";
 
-const required = (value) => {
-    if (!value) {
-        return (
-            <div className="alert alert-danger" role="alert">
-                This field is required
-            </div>
-        );
-    }
-};
-
 // Authentication and Login https://www.bezkoder.com/react-jwt-auth/
 
 function Login(props) {
@@ -33,10 +23,6 @@ function Login(props) {
                 }
                 navigate("/home");
             });
-    };
-
-    const getCurrentUser = (e) => {
-        return JSON.parse(localStorage.getItem("user"));
     };
 
     return (
@@ -68,7 +54,7 @@ function Login(props) {
                 <div>
                     <button type="submit">Login</button>
                     <p>
-                        No Account? <a href="#">Sign up</a>
+                        No Account? <a href="/signup">Sign up</a>
                     </p>
                 </div>
             </form>
