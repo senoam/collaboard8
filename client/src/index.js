a2f49493486f2398e103082d70ef43d56001d358
tonyq <tony_quan@sfu.ca>
2021-12-07 16:44:21 -0800

    Remove merge artifacts

--- client/src/index.js
+++ client/src/index.js
@@ -6,20 +6,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Whiteboard from "./routes/whiteboard/Whiteboard";
-<<<<<<< HEAD
-=======
-import HistoryCarousel from "./routes/history/Carousel";
->>>>>>> d1a90ff5d8e093e16908c89f99765a6578f51931
 import Login from "./routes/login/login";
 
 ReactDOM.render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login />}></Route>
-<<<<<<< HEAD
-=======
-            <Route path="/whiteboard/history" element={<HistoryCarousel />} />
->>>>>>> d1a90ff5d8e093e16908c89f99765a6578f51931
             <Route path="/whiteboard" element={<Whiteboard />} />
             <Route path="/home" element={<App />} />
         </Routes>
